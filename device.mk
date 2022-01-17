872acc3a25b17ed814c190905e742eb31dc13fe1
MikeQu223 <63991532+MikeQu223@users.noreply.github.com>
2022-01-17 18:49:24 +0800

    ginkgo: remove MIUIcamera

--- device.mk
+++ device.mk
@@ -13,7 +13,6 @@ DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay
 
 PRODUCT_PACKAGES += \
-    GinkgoMiuiCamera \
     GinkgoNoCutoutOverlay \
     NotchBarKiller \
     WillowFrameworkRes
@@ -88,7 +87,8 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.bluetooth.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth.xml
 
 # Camera
-$(call inherit-product, vendor/miuicamera/config.mk)
+PRODUCT_PACKAGES += \
+    GCamGOPrebuilt
 
 PRODUCT_PACKAGES += \
     android.frameworks.displayservice@1.0.vendor \
